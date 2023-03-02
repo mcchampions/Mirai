5b5d390725ab392fc1ac385a93eb0af2f0f5d4e7
Dreeam <61569423+Dreeam-qwq@users.noreply.github.com>
2023-03-01 22:55:20 -0500

    [ci skip] Update paperweight & dependency & build link

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "8.0.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.1"
+    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.3-SNAPSHOT"
 }
 
 repositories {
